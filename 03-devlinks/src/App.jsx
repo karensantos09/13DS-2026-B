ece7599f5c1c110b942ecb0476231c0f2cc1f4a3
karensantos09 <karen.c.santos8@aluno.senai.br>
2026-02-13 16:55:26 -0300

    Adicionar componente Switch e implementar tema claro/escuro; atualizar estilos globais

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,19 +1,26 @@
+import { useState } from "react";
 import "./App.css"
 import Link from "./components/Link/Link";
 import Perfil from "./components/Perfil/Perfil"
 import Rodape from "./components/Rodape/Rodape";
 import SocialLink from "./components/SocialLink/SocialLink";
+import Switch from "./components/Switch/Switch";
 
 
 
 function App() {
+  const[isLight,setIsLight] = useState(true)
+
+  const troca = () => {
+    setIsLight(!isLight);
+  };
+ 
+
   return (
-   <div id="App">
+    <div id="App" className={isLight ? "light" : ""}>
+   <Perfil fotoPerfil= {"https://placehold.co/100"}>Karen Santos</Perfil>
 
-<Perfil fotoPerfil= {"https://placehold.co/100"}>Karen Santos</Perfil>
-
-<div className="switch">botão switch</div>
-
+  <Switch troca={troca} isLight={isLight} />
 <div id="Link">
 <ul>
   <Link url={""}>Inscreva-se!</Link>
