955b673f5080c5e5a1a66ebbfe33d605e4a6399e
karensantos09 <karen.c.santos8@aluno.senai.br>
2026-02-19 11:18:40 -0300

    feat: adicionar componente Rodape e implementar busca com ícone

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,11 +1,27 @@
 import "./App.css";
 
+import logo from "./assets/devflix.png";
+import lupa from "./assets/search.svg";
+import Rodape from "./components/Rodape/Rodape";
+
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div id="App">
+      <img
+        id="logo"
+        src={logo}
+        alt="Logo original do serviço de streaming DEVFLIX com letras vermelhas e fundo preto, ideal para quem busca entretenimento digital de qualidade."
+      />
 
-export default App
+      <div className="search">
+        <input type="text" placeholder="Pesquise por filmes e séries" />
+        <img role="button" src={lupa} alt="Botão de ação para pesquisa!" />
+      </div>
+
+     <Rodape>KarenSantos</Rodape>
+
+    </div>
+  );
+};
+
+export default App;
