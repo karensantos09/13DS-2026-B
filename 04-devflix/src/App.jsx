3290a2ea1da0b8bae5e955b467fc94664954e9d0
karensantos09 <karen.c.santos8@aluno.senai.br>
2026-02-19 17:05:34 -0300

    Atualizar dependência do Vite para a versão 7.3.1 e configurar o servidor para usar a porta 3000

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,14 +1,36 @@
+import { useState } from "react";
 import "./App.css";
 
 import logo from "./assets/devflix.png";
 import lupa from "./assets/search.svg";
+
 import Rodape from "./components/Rodape/Rodape";
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  //Utilizando uma CHAVE de API do arquivo .env
+  const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+  const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
+
+
+  //Criando a conexão com a API e trazendo informações.
+ const searchMovies= async (title) => {
+  const response = await fetch(`${apiUrl}&s=${title}`);
+   const data = await response.json;
+
+//Alimentando a váriavel movies
+setMovies(data.Search);
+ }
+
+ useEffect(( =>{
+  searchMovies("Batman");
+ }, [] );
+
   return (
     <div id="App">
       <img
-        id="logo"
+        id="Logo"
         src={logo}
         alt="Logo original do serviço de streaming DEVFLIX com letras vermelhas e fundo preto, ideal para quem busca entretenimento digital de qualidade."
       />
@@ -18,8 +40,15 @@ const App = () => {
         <img role="button" src={lupa} alt="Botão de ação para pesquisa!" />
       </div>
 
-     <Rodape link={"https://github.com/karensantos09"}>KarenSantos</Rodape>
 
+<div className="container">
+{movies.map(movie, index) =>(
+
+  
+)}
+</div>
+
+      <Rodape link={"https://github.com/karensantos09"}>KarenSantos</Rodape>
     </div>
   );
 };
