9da1232cf45355e7e741469bc01b1ef207de0bb8
karensantos09 <karen.c.santos8@aluno.senai.br>
2026-02-20 15:54:40 -0300

    feat: melhorar estilos e responsividade do componente MovieCard e implementar busca de filmes

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -9,6 +9,7 @@ import MovieCard from "./components/MovieCard/MovieCard";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
 
   //Utilizando uma CHAVE de API do arquivo .env
   const apiKey = import.meta.env.VITE_OMDB_API_KEY;
@@ -24,7 +25,7 @@ const App = () => {
   };
 
   useEffect(() => {
-    searchMovies("Mickey Mouse");
+    searchMovies(""); /*Termo para pesquisa, ao carregar o site*/
   }, []);
 
   return (
@@ -36,8 +37,17 @@ const App = () => {
       />
 
       <div className="search">
-        <input type="text" placeholder="Pesquise por filmes" />
-        <img src={lupa} alt="Botão de ação para pesquisa!" />
+        <input
+        onKeyDown={(e) => e.key === "Enter" && searchMovies(search)}
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="Pesquise por filmes"
+        />
+        <img
+          onClick={() => searchMovies(search)}
+          src={lupa}
+          alt="Botão de ação para pesquisa!"
+        />
       </div>
 
       {movies?.length > 0 ? (
@@ -50,7 +60,7 @@ const App = () => {
         <h2 className="empty">Filme não encontrado 😭</h2>
       )}
 
-      <Rodape link={"https://github.com/ProfCastello"}>KarenSantos</Rodape>
+      <Rodape link={"https://github.com/karensantos09"}>KarenSantos</Rodape>
     </div>
   );
 };
