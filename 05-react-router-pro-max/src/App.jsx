3ed0efbd895ce01a250da286591fba41c2cc3dae
karensantos09 <karen.c.santos8@aluno.senai.br>
2026-03-12 16:53:45 -0300

    feat: inicializar projeto React com roteamento e estrutura básica
    - Adicionado package.json com dependências para React, React Router e Vite.
    - Criada estrutura principal da aplicação com componente App e configuração de roteamento.
    - Implementados componentes Header e Footer para navegação e conteúdo do rodapé.
    - Desenvolvidas páginas: Home, Sobre, Contato e NaoEncontrado com conteúdo básico.
    - Adicionado formulário na página Contato para entrada de dados do usuário.
    - Incluído CSS para estilização e ajustes de layout.
    - Configurado Vite para servidor de desenvolvimento e processo de build.

---
+++ 05-react-router-pro-max/src/App.jsx
@@ -0,0 +1,32 @@
+import { Route, Routes } from "react-router";
+import "./App.css";
+
+import Sobre from "./pages/Sobre";
+import Home from "./pages/Home";
+import Contato from "./pages/Contato";
+import NaoEncontrado from "./pages/NaoEncontrado";
+import Header from "./components/header/Header";
+import Rodape from "./components/rodape/Rodape";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="containerApp">
+        <Routes>
+          {/* Identifica todas as rotas do sistema*/}
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<Sobre />} /> {/* Uma rota do sistema*/}
+          <Route path="/contato" element={<Contato />} />
+          {/* Uma rota do sistema*/}
+          <Route path="/*" element={<NaoEncontrado />} />
+          {/* Uma rota do sistema*/}
+        </Routes>
+      </div>
+
+      <Rodape link={"https://github.com/karensantos09"}>Karen Santos</Rodape>
+    </>
+  );
+}
+
+export default App;
